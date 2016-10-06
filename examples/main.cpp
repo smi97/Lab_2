681092ead5c27f0354bee976374fc2ded6a805f0
Max Sudarev <smi97@mail.ru>
2016-10-06 16:42:58 +0300

    Update main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,5 +1,20 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include <iostream>
 #include <print_hello_world.h>
+
+int incr(int a)
+{
+    return a++;
+}
+
 int main(){
     print_hello_world();
+}
+
+TEST_CASE( "Increments s are computed", "[increment]" ) {
+    REQUIRE( incr(1) == 2 );
+    REQUIRE( incr(2) == 3 );
+    REQUIRE( incr(3) == 4 );
+    REQUIRE( incr(10) == 11 );
 }
