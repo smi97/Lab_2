1b0f3f310209509eff6159aff588ab0e41227a16
Max Sudarev <smi97@mail.ru>
2016-10-06 17:05:35 +0300

    Add files via upload

---
+++ Tests/print_hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../surces/incr.cpp"
+
+
+TEST_CASE( "Increments are computed", "[increment]" ) {
+    REQUIRE( incr(1) == 2 );
+    REQUIRE( incr(2) == 3 );
+    REQUIRE( incr(3) == 4 );
+    REQUIRE( incr(10) == 11 );
+}
